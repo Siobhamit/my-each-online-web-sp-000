5d8f3e66ae8c2e07a1f9394896da42dae4207a2a
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-05-04 10:27:30 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)# put argument(s) here
+if block_given?
+  i = 0
+
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+
+  array
+else
+  puts "This block should not run!"
+end
 end
